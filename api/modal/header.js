dacf9e6338566afa8facab9b50c1baa014a2a9df
mvulanemotha <88075973+mvulanemotha@users.noreply.github.com>
2023-04-24 09:38:01 +0200

    new updates

--- api/modal/header.js
+++ api/modal/header.js
@@ -4,19 +4,19 @@ dotenv.config();
 
 
 let tellerHeaders = (username, password) => {
-  
+
   let apiKey = process.env.key;
-  
+
   let musonCredentials = username + ':' + password
-  
+
   let buff = new Buffer.from(musonCredentials)
-  
+
   let base64Credentials = buff.toString('base64');
-  
+
   let authHeader = 'Basic ' + base64Credentials
-  
+
   //let token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64')
-  
+
   return header = {
     //'Authorization': 'Basic U0NCUzAwMTA6TXZ1bGFuZTJAQA==',//`Basic ${token}`,
     //'User-Agent': 'PostmanRuntime/7.29.2',
@@ -27,7 +27,7 @@ let tellerHeaders = (username, password) => {
     'keep-alive': true
     //'Accept-Encoding' : 'gzip, deflate, br'
   }
-  
+
   /*uUUnYBrEJ48mp2JoM8Jni7yIf8JDRP1N7mZvUTmO
   Basic U0NCUzAwMTA6TXZ1bGFuZTJAQA==*/
 
@@ -35,14 +35,14 @@ let tellerHeaders = (username, password) => {
 
 let headers = () => {
 
-  let apiKey = process.env.key;
-  let username = process.env.User
-  let password = process.env.password
+  let apiKey = 'uUUnYBrEJ48mp2JoM8Jni7yIf8JDRP1N7mZvUTmO' //process.env.key;
+  let username = 'Status Capital API' //process.env.User
+  let password = 'Status12345' //process.env.password
 
-  
+
 
   let musonCredentials = username + ':' + password
-  
+
   let buff = new Buffer.from(musonCredentials)
 
   let base64Credentials = buff.toString('base64');
--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const express = require('express')
 const app = express();
 const ussd = require('./api/routes/index')
 const bodyParser = require('body-parser')
+var cors = require('cors')
 
-//app.use(bodyParser.json())
+
+
+app.use(cors())
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 //app.use(morgan('dev'))
 
-
 app.use((req, res, next) => {
 
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
     res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-type,Accept,X-Access-Token,X-Key");
 
     // Set response contenttype
