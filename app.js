dcbc103d352b8263485fdfc1a8024bfe9c986959
mvulanemotha <88075973+mvulanemotha@users.noreply.github.com>
2023-04-25 11:58:14 +0200

    new updates

--- app.js
+++ app.js
@@ -2,11 +2,32 @@ const express = require('express')
 const app = express();
 const ussd = require('./api/routes/index')
 const bodyParser = require('body-parser')
+const morgan = require('morgan');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-//app.use(express.json())
+app.use(morgan('dev'))
+
+app.use((req, res, next) => {
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', '*');
+    
+    res.set('Freeflow: FC');
+    res.set('Content-Type : text/plain');
+
+    //res.set(200,{ 'Content-Type': 'text/plain', 'Freeflow': 'FC' });
+
+    if ('OPTIONS' == req.method) {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+})
+
+app.use(express.json())
 
 app.use('/ussd', ussd)
 
